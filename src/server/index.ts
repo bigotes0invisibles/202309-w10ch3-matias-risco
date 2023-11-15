416614b7bdb35372f8a7890f5aee7caf4ca5a36f
Matias Risco <matiasriscolagos@gmail.com>
2023-11-15 22:58:06 +0100

    Add router in items

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,8 +3,10 @@ import express from "express";
 import app from "./app.js";
 import { notFound } from "./middleware/error/errorMiddleware.js";
 import pingRouter from "../features/ping/router/pingRouter.js";
+import itemsRouter from "../features/items/router/itemsRouter.js";
 
 app.use(express.json());
 app.use(morgan("dev"));
+app.use("/items", itemsRouter);
 app.use("/", pingRouter);
 app.use(notFound);
