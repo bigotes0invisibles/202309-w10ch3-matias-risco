f698fe35a88fa1b8cd046d362db611525f28e2e9
Matias Risco <matiasriscolagos@gmail.com>
2023-11-15 22:46:05 +0100

    Add database conection

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
+import chalk from "chalk";
 import { startServer } from "./server/app.js";
 import "./server/index.js";
+import conecToDatabase from "./database/index.js";
 
 const port = process.env.PORT ?? 4000;
+if (!process.env.MONGODB_URL) {
+  console.log(chalk.red("MissingMongoDB Connecting String"));
+  process.exit();
+}
 
+const mongoUrl = process.env.MONGODB_URL;
+
+await conecToDatabase(mongoUrl);
 startServer(+port);
