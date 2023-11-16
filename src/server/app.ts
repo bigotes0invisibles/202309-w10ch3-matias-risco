25ae24cc9e1b3511275bbd280f69b656eccccc1b
Matias Risco <matiasriscolagos@gmail.com>
2023-11-16 19:29:53 +0100

    Fix security  Hotspots

--- src/server/app.ts
+++ src/server/app.ts
@@ -3,12 +3,13 @@ import express from "express";
 import chalk from "chalk";
 
 const app = express();
+app.disable("x-powered-by");
 
 export const startServer = (port: number) => {
   app.listen(+port, () => {
     console.log(
       chalk.blue(
-        `lisening in port ${chalk.bgMagenta(`http://localhost:${port}/`)}`,
+        `lisening in port` + chalk.bgMagenta(`http://localhost:${port}/`),
       ),
     );
   });
