6aa43788d3271967194e072e8ffd50435d570b3e
Matias Risco <matiasriscolagos@gmail.com>
2023-11-16 16:22:42 +0100

    Add ItemsRepository as param in contructor of ItemsController

--- src/features/items/router/itemsRouter.ts
+++ src/features/items/router/itemsRouter.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import ItemsController from "../controller/ItemsController.js";
+import ItemsRepository from "../repository/ItemsRepository.js";
 
-const itemsController = new ItemsController();
+const itemsRepository = new ItemsRepository();
+const itemsController = new ItemsController(itemsRepository);
 const itemsRouter = Router();
 
 itemsRouter.get("/items", itemsController.getThings);
